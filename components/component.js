578d60fbce032bafe9b78e1c39ddcd52e1b04419
nick <nmdufour@gmail.com>
2018-06-15 13:07:16 -0400

    added http request

--- components/component.js
+++ components/component.js
@@ -1,14 +1,32 @@
 "use strict";
+
 const component = {
-    template: `
-    `,
-    controller: [MuseumService, function(MuseumService) {
-        const vm = this;
-    }]
+  template: `
+  <div ng-repeat="item in $ctrl.artList track by $index">
+    <img ng-src="https://ids.lib.harvard.edu/ids/view/{{item.images[0].idsid}}">
+    <p>{{ item.title }}</p>
+    <p>{{ item.period }}</p>
+    <p>{{ item.department }}</p>
+    <p>{{item.accessionyear}}</p>
+
+  </div>
+  `,
+  controller: ["TestService", function (TestService){
+    const vm = this;
+    vm.artList = [];
+    TestService.getInfo().then((response) =>{
+      for (let i = 0; i < 15; i++) {
+        if (response.data.records[i].imagecount > 0 || response.data.records[i].images === 0){
+          vm.artList.push(response.data.records[i]);
+          console.log(response.data.records[i]);
+        }
+      }
+    })
+
+  }]
+  // response.data.records[i].imagecount > 0
 }
 
-
-
 angular
-    .module("app")
-    .component("component", omponent);
+  .module("app")
+  .component("component", component)
--- service.js
+++ service.js
@@ -1,11 +1,22 @@
 "use strict";
-function MuseumService$(http) {
 
+function TestService($http) {
+  let artList = [];
+  const getInfo = () =>{
+    return $http({
+      method: 'GET',
+      url: 'https://api.harvardartmuseums.org/object?size=15&page=4&apikey=88bb71d0-7015-11e8-9d38-6fd658e729d6'
+    }).then((response)=>{
+      artList = response;
+      return artList;
+    })
+  }
+
+  return {
+    getInfo
+  }
 }
 
-return {
-    
-}
-angular 
-    .module("app")
-    .factory("MuseumService", MuseumService);
+angular
+  .module("app")
+  .factory("TestService", TestService);
