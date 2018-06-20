781819114bee1c5991e7e121b38d9c7d00a6e86c
frances <frankie.rae1966@gmail.com>
2018-06-20 16:25:54 -0400

    type-comp routing

--- components/images-comp.js
+++ components/images-comp.js
@@ -1,33 +1,20 @@
 "use strict";
-console.log("image comp here"); 
 const imagesComponent = {
+
   template: `
-
-  <div class="image" ng-repeat="item in $ctrl.artList track by $index">
-    <img ng-src="https://ids.lib.harvard.edu/ids/view/{{item.images[0].idsid}}">
-    <p>{{ item.title }}</p>
-    <p>{{ item.period }}</p>
-    <p>{{ item.department }} </p>
-    <p> {{ item.accessionyear }} </p>
-    <p> {{ item.medium }} </p> 
-    <p> {{ item.division }} </p> 
-    <p> {{ item.description}} </p> 
+  <div ng-repeat="item in $ctrl.testItems track by $index" ng-show="item.images[1]">
+    <h3>{{ item.title }}</h3>
+    <img ng-src="{{item.images[0].baseimageurl}}">
   </div>
-
   `,
-  controller: ["TestService", function (TestService){
-    const vm = this;
-    vm.artList = [];
-    TestService.getInfo().then((response) =>{
-      // for (let i = 0; i < 100; i++) {
-      //   if (response.data.records[i].imagecount > 0){
-      //     vm.artList.push(response.data.records[i]);
-          // console.log(response.data.records);
-        }
-    )
+  controller: function(TestService){
+    const vm = this; 
+    // vm.images = function(){
+      vm.testItems = TestService.returnClassificationImages();
+    // }
+    // vm.images();
 
-  }]
-  // response.data.records[i].imagecount > 0
+  }
 }
 
 angular
@@ -44,4 +31,18 @@ angular
     <p>{{ item.accessionyear }}</p>
     <p> {{ item.medium }} </p> 
   </div>
+
+    // controller: ["TestService", function (TestService){
+  //   const vm = this;
+  //   vm.artList = [];
+  //   TestService.getInfo().then((response) =>{
+  //     // for (let i = 0; i < 100; i++) {
+  //     //   if (response.data.records[i].imagecount > 0){
+  //     //     vm.artList.push(response.data.records[i]);
+  //         // console.log(response.data.records);
+  //       }
+  //   )
+
+  // }]
+  // // response.data.records[i].imagecount > 0
   */
--- components/type-comp.js
+++ components/type-comp.js
@@ -17,23 +17,14 @@ const typeComponent = {
     <button type="button" ng-click="$ctrl.displayClass('Vessels');">Vessels</button>
   </section>
 
-  <div ng-repeat="item in $ctrl.classList track by $index" ng-show="item.images[1]">
-    <h3>{{ item.title }}</h3>
-    <img ng-src="{{item.images[0].baseimageurl}}">
-  </div>
+  
   `,
-  controller: ["TestService", function(TestService){
+  controller: ["$location", "TestService", function($location,TestService){
     const vm = this; 
-    vm.classList = [];
     vm.displayClass = function (classType) {
-    TestService.getClassification(classType).then((response) => {
-      vm.artInfo = response;
-      vm.classList = [];
-      for (let i= 0; i < 100; i++) {
-        vm.classList.push(response.data.records[i]);
-        console.log(response.data.records[i]);
-      }
-      })
+      TestService.getClassification(classType).then(() => {
+        $location.path("/images-comp");
+      });
     } 
   }]
 }
