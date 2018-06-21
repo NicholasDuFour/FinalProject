9b2baf233451cb864bff2ae7ed7922cf86b14c37
amanda <am.morang@gmail.com>
2018-06-21 16:13:50 -0400

    deleted a function

--- components/color-comp.js
+++ components/color-comp.js
@@ -3,9 +3,6 @@
 const colorComponent = {
   template: `
   <section class="buttoncontainer">
-  <button type="button" ng-click="$ctrl.searchColorInfo();">ColorGetRequest</button>
-
-
   <button type="button" ng-click="$ctrl.nextColorPage();">Next Page</button>
 
     <button type="button" ng-click="$ctrl.colorFinder('Red');">Red</button>
