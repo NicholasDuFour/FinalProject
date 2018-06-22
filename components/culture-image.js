891ad1ae2b8cddf64e6e3ba70ca7fdec8598ad03
nick <nmdufour@gmail.com>
2018-06-22 16:55:40 -0400

    added more info section and more results from GET req

--- components/culture-image.js
+++ components/culture-image.js
@@ -4,8 +4,20 @@ console.log("culture image!");
 const cultureImage = {
     template: `
     <div class="typefiles" ng-repeat="item in $ctrl.cultureRepeat track by $index" ng-show="item.images[1]">
-        <h3>{{ item.title }}</h3>
+
         <img ng-src="{{item.images[0].baseimageurl}}">
+        <button ng-click="showme=true">Learn More</button>
+
+        <section ng-show="showme">
+          <p ng-click="showme=false">Close</p>
+          <h3>{{ item.title }}</h3>
+          <p>Artist: {{ item.people[0].name }}</p>
+          <p>Date: {{item.dated}}</p>
+          <p>Type: {{item.classification}}</p>
+          <p>Medium: {{item.medium}}</p>
+          <p>{{item.division}}</p>
+        </section>
+
     </div>
     `,
 
