ac48ad5c7986c736ff46a8a1849f39b63d4fd156
Charity Cardinal <charcard@umich.edu>
2018-06-23 16:39:42 -0400

    Fixed button styling

--- components/home-comp.js
+++ components/home-comp.js
@@ -13,34 +13,20 @@ const homeComponent = {
 
 <section class="buttoncontainer">
     <a href="#!/color-comp" div class="imagebuttoncontainer"</a>
-        <img src="images/paint.jpg" alt="Paint">
+        <img src="images/lotsofcolors.jpg" alt="Paint">
     <div class="imagebuttonoverlay"><p>color</p></div>
     </div>
     <a href="#!/type-comp" div class="imagebuttoncontainer"</a>
-        <img src="images/glass.jpg" alt="Stained Glass">
+        <img src="images/stained.jpg" alt="Stained Glass">
     <div class="imagebuttonoverlay"><p>type</p></div>
     </div>
     <a href="#!/culture-comp" div class="imagebuttoncontainer"</a>
-        <img src="images/lanterns.jpg" alt="Lanterns">
+        <img src="images/lantern.jpg" alt="Lanterns">
     <div class="imagebuttonoverlay"><p>culture</p></div>
     </div>
 
 
 </section>
-
-<section class="mainbuttons">
-
-
-        <a href="#!/color-comp" class="colorlink">color</a>
-
-    
-        <a href="#!/type-comp" class="typelink">type</a>
-
-
-        <a href="#!/culture-comp" class="culturelink">culture</a>
-
-
-  </section>
     `,
 }
 
