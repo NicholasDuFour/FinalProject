9a3299bd6ed278d5345b5e73e4fcad84c8a67952
Charity Cardinal <charcard@umich.edu>
2018-06-25 18:59:55 -0400

    fixing things that got deleted

--- components/home-comp.js
+++ components/home-comp.js
@@ -24,15 +24,15 @@ const homeComponent = {
     <section class="homebuttoncontainer">
     <a href="#!/color-comp" div class="homebutton"</a>
         <img src="images/colors.jpg" alt="Paint">
-    <div class="imagebuttonoverlay"><p>color</p></div>
+    <div class="imagebuttonoverlayhome"><p>color</p></div>
     </div>
     <a href="#!/type-comp" div class="homebutton"</a>
         <img src="images/stained-glass.jpg" alt="Stained Glass">
-    <div class="imagebuttonoverlay"><p>type</p></div>
+    <div class="imagebuttonoverlayhome"><p>type</p></div>
     </div>
     <a href="#!/culture-comp" div class="homebutton"</a>
         <img src="images/redlanterns.jpg" alt="Lanterns">
-    <div class="imagebuttonoverlay"><p>culture</p></div>
+    <div class="imagebuttonoverlayhome"><p>culture</p></div>
     </div>
     </section>
 `,
