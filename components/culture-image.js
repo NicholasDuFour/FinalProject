3916db4818cab286bd9886313ce97a90d54d1ad3
nick <nmdufour@gmail.com>
2018-06-25 20:53:05 -0400

    arranged arrow buttons on carousel

--- components/culture-image.js
+++ components/culture-image.js
@@ -11,12 +11,18 @@ const cultureImage = {
     </header>
 
     <section class="slideshow">
-        <div class="typefiles">
+    <section class="prev">
+      <i class="material-icons md-36 prev" ng-click="$ctrl.goBack();">arrow_back_ios</i>
+    </section>
+
+      <div class="typefiles">
         <img class="repimg" ng-src="{{ $ctrl.cultureRepeat[$ctrl.count].images[0].baseimageurl }}">
-        <i class="material-icons md-36 prev" ng-click="$ctrl.goBack();">arrow_back_ios</i>
-        <i class="material-icons md-36 next" ng-click="$ctrl.goForward();">arrow_forward_ios</i>
         <button class="learn" ng-hide="showme" ng-click="showme=true">Learn More</button>
-        </div>
+      </div>
+
+      <section class="next">
+        <i class="material-icons md-36 next" ng-click="$ctrl.goForward();">arrow_forward_ios</i>
+      </section>
     </section>
 
     <section ng-show="showme" class="moreinfo">
--- components/type-image-comp.js
+++ components/type-image-comp.js
@@ -9,12 +9,18 @@ const imagesComponent = {
   </header>
 
   <section class="slideshow">
+  <section class="prev">
+    <i class="material-icons md-36 prev" ng-click="$ctrl.goBack();">arrow_back_ios</i>
+  </section>
+
     <div class="typefiles">
       <img class="repimg" ng-src="{{ $ctrl.typeRepeat[$ctrl.count].images[0].baseimageurl }}">
-      <i class="material-icons md-36 prev" ng-click="$ctrl.goBack();">arrow_back_ios</i>
-      <i class="material-icons md-36 next" ng-click="$ctrl.goForward();">arrow_forward_ios</i>
       <button class="learn" ng-hide="showme" ng-click="showme=true">Learn More</button>
     </div>
+
+    <section class="next">
+      <i class="material-icons md-36 next" ng-click="$ctrl.goForward();">arrow_forward_ios</i>
+    </section>
   </section>
 
 
@@ -49,4 +55,3 @@ const imagesComponent = {
 angular
   .module("app")
   .component("imagesComponent", imagesComponent);
-
