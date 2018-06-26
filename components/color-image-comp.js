a9fb697c1e2e38f79a4ef52e44344b5a573da6dd
amanda <am.morang@gmail.com>
2018-06-26 15:12:46 -0400

    created a pop up with more info

--- components/color-image-comp.js
+++ components/color-image-comp.js
@@ -38,16 +38,20 @@ const colorImages = {
   <img ng-src="{{item.images[0].baseimageurl}}">
   <button class="learn" ng-click="showme=true">Learn More</button>
   <section ng-show="showme" class="moreinfo">
-      <section class="closebtn">
-        <i ng-click="showme=false" class="material-icons">close</i>
-      </section>
-    <img ng-src="{{item.images[0].baseimageurl}}">
+    <div class="left">
+      <img ng-src="{{item.images[0].baseimageurl}}">
+    </div>
+    <div class="right">
+    <section class="closebtn">
+      <i ng-click="showme=false" class="material-icons">close</i>
+    </section>
     <h4>{{ item.title || 'No title available' }}</h4>
     <p>Artist: {{ item.people[0].name || 'No artist available'  }}</p>
     <p>Date: {{ item.dated || 'No date available' }}</p>
     <p>Type: {{ item.classification || 'No description available' }}</p>
     <p>Medium: {{ item.medium || 'No description available' }}</p>
     <p>Division: {{ item.division || 'No description available' }}</p>
+    </div>
   </section>
   </div>
 </section>
